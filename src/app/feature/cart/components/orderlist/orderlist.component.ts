a16427bc54e092480739a56454e09853f993aa5c
Kaushlesh Pandey <akurp058@gmail.com>
2020-11-16 16:00:50 +0530

    Order List Work

--- src/app/feature/cart/components/orderlist/orderlist.component.ts
+++ src/app/feature/cart/components/orderlist/orderlist.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderlist.component.scss']
 })
 export class OrderlistComponent implements OnInit {
-
+  public counter = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addCounter() {
+    this.counter = this.counter + 1;
+    console.log(this.counter);
+    return this.counter;
+  }
+
+  removeCounter(counter) {
+    if (this.counter > 0) {
+      this.counter = counter - 1;
+      console.log(this.counter);
+      return this.counter;
+    }
+  }
 }
